5541acef6c731c94eb6a8cf3dc1ba56d45bbc597
adam-sv-uwq <afanasevadam19@gmail.com>
2025-06-29 23:28:35 +0500

    comm1

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.os.Handler  // Добавьте этот импорт
+import android.content.Intent  // Добавьте этот импорт
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, StartActivity::class.java)
+            startActivity(intent) // Исправлено: используйте startActivity вместо StartActivity
+            finish()
+        }, 2000)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/StartActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityStartBinding // Импортируйте с правильным именем пакета
+
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater) // Создание привязки
+        enableEdgeToEdge()
+        setContentView(binding.root) // Убедитесь, что мы используем binding.root
+
+        // Измените здесь целевой класс на MainActivity
+        binding.goUserLoginBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
